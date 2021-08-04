462a59778ea2fd953a90d179a12ea36fc080445b
Ruchi Yadav <ruchiy2410@gmail.com>
2021-08-04 23:03:27 +0530

    my first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import AddItemForm from './AddItemForm';
+import './App.css';
+import ItemList from './ItemList';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const addItem = (item) => {
+    // assuming no duplicates for demo purposes
+    setItems([...items, item]);
+  };
+
+  const removeItem = (itemToBeDeleted) => {
+    setItems(items.filter((item) => itemToBeDeleted !== item));
+  };
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        To Do List
+        <ItemList items={items} removeItem={removeItem} />
+        <AddItemForm addItem={addItem} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
